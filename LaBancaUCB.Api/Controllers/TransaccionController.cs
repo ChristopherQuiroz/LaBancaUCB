6dde6f408e4755fd09c152b939237bbd69c8a429
Victor <victormancilla@ucb.edu.bo>
2026-03-30 18:57:15 -0400

    Implementacion finalizada de CU-01, CU-02, CU-06 y CU-07 con JWT

---
+++ LaBancaUCB.Api/Controllers/TransaccionController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using LaBancaUCB.Api.Response;
+using LaBancaUCB.Core.DTOs;
+using LaBancaUCB.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace LaBancaUCB.Api.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class TransaccionController : ControllerBase
+{
+    private readonly ITransaccionService _transaccionService;
+    private readonly IMapper _mapper;
+
+    public TransaccionController(ITransaccionService transaccionService, IMapper mapper)
+    {
+        _transaccionService = transaccionService;
+        _mapper = mapper;
+    }
+
+    [HttpGet("historial")]
+    public async Task<ActionResult> GetHistorial()
+    {
+        try
+        {
+            var idUsuarioClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(idUsuarioClaim) || !long.TryParse(idUsuarioClaim, out long idUsuario))
+            {
+                return Unauthorized(new { message = "Token inválido o usuario no identificado" });
+            }
+
+            var transacciones = await _transaccionService.GetHistorialByUsuarioIdAsync(idUsuario);
+            var transaccionesDto = _mapper.Map<IEnumerable<TransaccionDto>>(transacciones);
+
+            var response = new ApiResponse<IEnumerable<TransaccionDto>>(transaccionesDto);
+            return Ok(response);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new
+            {
+                message = "Error al consultar el historial de movimientos",
+                error = ex.Message
+            });
+        }
+
+    }
+
+    [Authorize(Roles = "admin")] 
+    [HttpGet("historial/{idUsuario}")]
+    public async Task<ActionResult> GetHistorialPorCliente(long idUsuario)
+    {
+        try
+        {
+            var transacciones = await _transaccionService.GetHistorialByUsuarioIdAsync(idUsuario);
+
+            var transaccionesDto = _mapper.Map<IEnumerable<TransaccionDto>>(transacciones);
+
+            var response = new ApiResponse<IEnumerable<TransaccionDto>>(transaccionesDto);
+            return Ok(response);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new
+            {
+                message = "Error al consultar el historial del cliente",
+                error = ex.Message
+            });
+        }
+    }
+}
